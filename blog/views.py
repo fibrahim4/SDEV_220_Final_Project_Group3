37e37a7bf367affb76bbd451bb7d1d9a1677779b
kesny <kraingsey@ivytech.edu>
2024-04-29 11:22:28 -0400

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+#this adds a inventory list function the defer to the post list html page. we respond to a request, send to the browser
+def inventory_list(request):
+    return render(request, 'blog/inventory_list.html', {})
