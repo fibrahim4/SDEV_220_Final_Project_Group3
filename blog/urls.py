f0e09525cdb9e46d6de051c71657a02be7d2d674
kesny <kraingsey@ivytech.edu>
2024-05-10 17:17:39 -0400

    Added Signin, Signup, Signout

--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.inventory_list, name='inventory_list'),
+    path('', views.inventory_shop, name='inventory_shop'),
+    path('inventory/list/', views.inventory_list, name='inventory_list'),
     path('inventory/<int:pk>/', views.inventory_detail, name='inventory_detail'),
     path('inventory/new/', views.inventory_new, name='inventory_new'),
     path('inventory/<int:pk>/edit/', views.inventory_edit, name='inventory_edit'),
-    path('inventory/order/', views.inventory_order, name='inventory_order'),
+    path('inventory/<pk>/remove/', views.inventory_remove, name='inventory_remove'),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
+    path('signout', views.signout, name='signout'),
+ 
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,18 +3,29 @@ from django.shortcuts import render, get_object_or_404
 from .models import Inventory
 from .inventory_input import InventoryInput
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
 #this adds a inventory list function the defer to the post list html page. we respond to a request, send to the browser
+@login_required
 def inventory_list(request):
-    Inventorys = Inventory.objects.filter(status='In Stock')
+    Inventorys = Inventory.objects.all()
     return render(request, 'blog/inventory_list.html', {'Inventorys': Inventorys})
 
+def inventory_shop(request):
+    Inventorys = Inventory.objects.filter(status='In Stock')
+    return render(request, 'blog/inventory_shop.html', {'Inventorys': Inventorys})
+
+@login_required
 def inventory_detail(request, pk):
     inventory = get_object_or_404(Inventory, pk=pk)
     return render(request, 'blog/inventory_detail.html', {'inventory' :inventory})
 
+@login_required
 def inventory_new(request):
     if request.method == "POST":
         form = InventoryInput(request.POST)
@@ -27,6 +38,7 @@ def inventory_new(request):
         form = InventoryInput()
     return render(request, 'blog/inventory_edit.html', {'form': form})
 
+@login_required
 def inventory_edit(request, pk):
     inventory = get_object_or_404(Inventory, pk=pk)
     if request.method == "POST":
@@ -41,7 +53,55 @@ def inventory_edit(request, pk):
     return render(request, 'blog/inventory_edit.html', {'form': form})
 
 
-def inventory_order(request):
-    Inventorys = Inventory.objects.filter(status='In Stock')
-    return render(request, 'blog/inventory_order.html', {'Inventorys': Inventorys})
+
+@login_required
+def inventory_remove(request, pk):
+    invenotry = get_object_or_404(Inventory, pk=pk)
+    invenotry.delete()
+    return redirect('inventory_list')
+
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST.get('username')      
+        fname = request.POST.get('fname')
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+        
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        
+        myuser.save()
+        
+        messages.success(request, "Your Account has been successfully created.")
+        
+        return redirect('inventory_shop')
+
+    return render(request, "registration/signup.html")
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        passs1 = request.POST['pass1']
+        
+        user = authenticate(username=username, password=passs1)
+        
+        if user is not None:
+            login(request, user)
+            fname = user.first_name
+            return redirect('inventory_shop')
+            
+        else:
+            messages.error(request, "Bad Credentials!")
+            return redirect('inventory_shop')
+        
+    return render(request, "registration/signin.html")
+
+def signout(request):
+    logout(request)
+    messages.success(request, "Logged Out Successfully!")
+    return redirect('inventory_shop')
 
